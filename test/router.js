eb49c9d79fc004b6f6284c5ddbc08bcb0696ec9e
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2012-12-08 12:05:59 +0200

    add tests to check if paginate, fields and filters work

--- test/router.js
+++ test/router.js
@@ -186,11 +186,87 @@ describe( 'Router.js', function () {
                 assert.equal(res.body.name, that.author.name);
                 assert.equal(res.body.books.length, 3);
                 for (var i = 0, n = res.body.books.length; i<n; i ++) {
-                    debugger;
                     var book = that['book'+(i+1)];
                     var bookIsRetrieved = res.body.books.indexOf(book.id) >= 0;
                     assert.equal(true, bookIsRetrieved);
                 }
+                return done();
+            });
+    });
+
+
+
+	it('GET /api/v1/books?offset=1&limit=2', function (done) {
+        // Check if pagination works.
+        var that = this;
+        var offset = 1;
+        var limit = 2;
+        request(app)
+            .get('/api/v1/books?offset='+offset+'&limit='+limit)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .end( function (err, res) {
+                if (err) return done(err);
+
+                assert.equal(res.statusCode, 200);
+                assert.equal(res.body.length, 2);
+
+                var expected1 = that.book2
+                var expected2 = that.book3
+                var book1 = res.body[0]
+                var book2 = res.body[1]
+
+                assert.equal(book1.id, expected1.id.toString());
+                assert.equal(book1.title, expected1.title);
+                assert.equal(book1.author, expected1.author.toString());
+
+                assert.equal(book2.id, expected2.id.toString());
+                assert.equal(book2.title, expected2.title);
+                assert.equal(book2.author, expected2.author.toString());
+
+                return done();
+            });
+	});
+
+
+    it('GET /api/v1/books?title=War and Peace', function (done) {
+        // Check if filters work by retrieving a book with a certain title.
+        var expected = this.book1;
+        request(app)
+            .get('/api/v1/books?title='+encodeURIComponent(expected.title))
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .end( function (err, res) {
+                if (err) return done(err);
+
+                assert.equal(res.statusCode, 200);
+                assert.equal(res.body.length, 1);
+
+                var book = res.body[0];
+                assert.equal(book.id, expected.id.toString());
+                assert.equal(book.title, expected.title);
+                assert.equal(book.author, expected.author.toString());
+
+                return done();
+            });
+    });
+
+
+    it('GET /api/v1/books/:bookId?fields=title', function (done) {
+        // Check if selecting certain fields from a resource works
+        var expected = this.book2;
+        request(app)
+            .get('/api/v1/books/'+expected.id.toString()+'?fields=title')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .end( function (err, res) {
+                if (err) return done(err);
+
+                assert.equal(res.statusCode, 200);
+                assert.equal(res.body.id, expected.id.toString(), 'should not be set');
+                assert.equal(res.body.title, expected.title, 'should be set');
+                assert.equal(res.body.author, undefined, 'should be set');
+
                 return done();
             });
     });
